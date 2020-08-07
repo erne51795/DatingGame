9894a55f35974055f4a457775fb94942d391aa2d
erne51795 <66523436+erne51795@users.noreply.github.com>
2020-08-07 10:05:17 -0500

    Add files via upload

---
+++ Mymessages.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _7_4DatingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DatingProfile Robert = new DatingProfile("Robert", "Williams", 50, "Male");
+            Robert.WriteBio("Strong outdoors type");
+
+            DatingProfile Barbara = new DatingProfile("Barbara", "Jones", 45, "Female");
+            Barbara.WriteBio("new to this site");
+
+            Robert.SendMessage("Hello Barbara", "Want to get some coffee?", Barbara);
+            Barbara.ReadMessage();
+        }
+    }
+}
